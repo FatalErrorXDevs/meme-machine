5cf3edc44e1ad3d0133a6392c535489028a95342
tom prins <tomprins@toms-MacBook-Pro.local>
2017-07-07 17:20:31 +0200

    added cases for future features

--- MessageHandler.js
+++ MessageHandler.js
@@ -7,7 +7,20 @@ class MessageHandler {
 
     handle(message) {
         if (message.author.username !== this.bot.user.username) {
-            message.reply(this.checkroleCommander(message));
+            if(message.content.startsWith('!add')){
+                message.reply("not implemented? " + message.content);
+            }
+            if(message.content.startsWith('!remove')){
+                message.reply("not implemented? " + message.content);
+            }
+            if(message.content.startsWith('!commands')){
+                message.reply("not implemented? " + message.content);
+            }
+            else{
+                if(message.content.startsWith('!')){
+                    message.reply("not implemented playing sounds");
+                }
+            }
         }
     }
     checkroleCommander(message) {
