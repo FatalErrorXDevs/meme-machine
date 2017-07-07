ee5d2bd1e616f3b78204965327ea97a62d125c7c
tom prins <tomprins@toms-MacBook-Pro.local>
2017-07-07 22:57:23 +0200

    sounds can now be removed, only users with the role bot commander can remove sounds!

--- MessageHandler.js
+++ MessageHandler.js
@@ -8,25 +8,30 @@ class MessageHandler {
 
     handle(message) {
         if (message.author.username !== this.bot.user.username) {
-            if(message.content.startsWith('!commands')){
+            if (message.content.startsWith('!commands')) {
                 message.author.send(Util.commandsList());
                 return;
             }
-            if(message.content.startsWith('!remove')){
-                message.reply("not implemented? " + message.content);
+            if (message.content.startsWith('!remove')) {
+                if(!this.checkroleCommander(message)){
+                    message.reply("You do not have permission to do that! Request Bot Commander role!");
+                    return;
+                }
+                const sound = message.content.replace('!remove ', '');
+                Util.removeSound(sound, message.channel);
                 return;
             }
-            if(message.content.startsWith('!add')){
+            if (message.content.startsWith('!add')) {
                 Util.addSounds(message.attachments, message.channel);
                 return;
             }
-            if(message.content.startsWith('!sounds')){
+            if (message.content.startsWith('!sounds')) {
                 const sounds = Util.getSounds();
                 message.author.send(sounds.map(sound => sound));
                 return;
             }
-            else{
-                if(message.content.startsWith('!')){
+            else {
+                if (message.content.startsWith('!')) {
                     message.reply("not implemented playing sounds");
                     return;
                 }
@@ -35,9 +40,9 @@ class MessageHandler {
     }
     checkroleCommander(message) {
         let role = message.guild.roles.find("name", "Bot Commander");
-        return this.hasRole(role,message)
+        return this.hasRole(role, message)
     }
-    hasRole(role,message) {
+    hasRole(role, message) {
         if (message.member.roles.has(role.id)) {
             return true;
         } else {
--- Util.js
+++ Util.js
@@ -2,69 +2,79 @@ const fs = require('fs');
 const https = require('https');
 const config = require('config');
 
-class Util{
-constructor(){
+class Util {
+    constructor() {
 
-}
-  commandsList() {
-    return [
-      '```',
-      '!commands              Show this message',
-      '!sounds                Show available sounds',
-      '!<sound>               Play the specified sound',
-      '!random                Play random sound',
-      '!stop                  Stop playing and clear queue',
-      '!add                   Add the attached sound',
-      '!rename <old> <new>    Rename specified sound',
-      '!remove <sound>        Remove specified sound',
-      '```'
-    ].join('\n');
-  }
+    }
+    commandsList() {
+        return [
+            '```',
+            '!commands              Show this message',
+            '!sounds                Show available sounds',
+            '!<sound>               Play the specified sound',
+            '!random                Play random sound',
+            '!stop                  Stop playing and clear queue',
+            '!add                   Add the attached sound',
+            '!rename <old> <new>    Rename specified sound',
+            '!remove <sound>        Remove specified sound',
+            '```'
+        ].join('\n');
+    }
     addSounds(attachments, channel) {
-    attachments.forEach(attachment => this._addSound(attachment, channel));
-  }
-
-
-  getSoundsWithExtension() {
-    const files = fs.readdirSync('sounds/');
-    let sounds = files.filter(sound => config.get('extensions').some(ext => sound.endsWith(ext)));
-    sounds = sounds.map(sound => ({ name: sound.split('.')[0], extension: sound.split('.')[1] }));
-    return sounds;
-  }
-
-  getSounds() {
-    const sounds = this.getSoundsWithExtension();
-    return sounds.map(sound => sound.name);
-  }
-
- _addSound(attachment, channel) {
-    if (attachment.filesize > config.get('size')) {
-      channel.send(`${attachment.filename.split('.')[0]} is too big!`);
-      return;
+        attachments.forEach(attachment => this._addSound(attachment, channel));
     }
 
-    if (!config.get('extensions').some(ext => attachment.filename.endsWith(ext))) {
-      channel.send('Sound has to be in accepted format!');
-      return;
-    }   
-
-    const filename = attachment.filename.split('.')[0];
-    if (this.getSounds().includes(filename)) {
-      channel.send(`${filename} already exists!`);
-      return;
+    removeSound(sound, channel) {
+        console.log(channel);
+    const file = `sounds/${sound}.mp3`;
+    try {
+      fs.unlinkSync(file);
+      channel.sendMessage(`${sound} removed!`);
+    } catch (error) {
+      channel.sendMessage(`${sound} not found!`);
     }
 
-    https.get(attachment.url, (response) => {
-      if (response.statusCode === 200) {
-        const file = fs.createWriteStream(`./sounds/${attachment.filename}`);
-        response.pipe(file);
-        channel.send(`${filename} added!`);
-      }
-    }).on('error', (error) => {
-      console.error(error);
-      channel.send('Something went wrong!');
-    });
-  }
-  
+    }
+    getSoundsWithExtension() {
+        const files = fs.readdirSync('sounds/');
+        let sounds = files.filter(sound => config.get('extensions').some(ext => sound.endsWith(ext)));
+        sounds = sounds.map(sound => ({ name: sound.split('.')[0], extension: sound.split('.')[1] }));
+        return sounds;
+    }
+
+    getSounds() {
+        const sounds = this.getSoundsWithExtension();
+        return sounds.map(sound => sound.name);
+    }
+
+    _addSound(attachment, channel) {
+        if (attachment.filesize > config.get('size')) {
+            channel.send(`${attachment.filename.split('.')[0]} is too big!`);
+            return;
+        }
+
+        if (!config.get('extensions').some(ext => attachment.filename.endsWith(ext))) {
+            channel.send('Sound has to be in accepted format!');
+            return;
+        }
+
+        const filename = attachment.filename.split('.')[0];
+        if (this.getSounds().includes(filename)) {
+            channel.send(`${filename} already exists!`);
+            return;
+        }
+
+        https.get(attachment.url, (response) => {
+            if (response.statusCode === 200) {
+                const file = fs.createWriteStream(`./sounds/${attachment.filename}`);
+                response.pipe(file);
+                channel.send(`${filename} added!`);
+            }
+        }).on('error', (error) => {
+            console.error(error);
+            channel.send('Something went wrong!');
+        });
+    }
+
 }
 module.exports = new Util();
