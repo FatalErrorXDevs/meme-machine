dc460720e9683e6b07579159b5a8161a6e1a318f
FatalErrorX <fatalerrorxdevs@gmail.com>
2018-01-28 00:03:21 +0100

    started on web GUI

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 const config = require('config');
-require('./SoundBot.js');
+require('./SoundBot.js')
+var http = require('http'),
+    express = require('express'),
+    fs = require('fs'),
+    path = require('path');
+
+var app = express();
+app.use(express.static(__dirname + '/components'));
+app.use(express.static(__dirname + '/app'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+var server = http.createServer(app);
+
+app.get('/', function(req, res) {
+    res.sendfile('app/index.html');
+});
 
 console.log('Use the following URL to let the bot join your server!');
 console.log(
     `https://discordapp.com/oauth2/authorize?client_id=${config.get('client_id')}&scope=bot`
 );
+
+server.listen(3001, function() {
+    console.log('Listening on port 3001...');    
+});
